57e6fab43429ee83f1d7dfbeb95da394e9982bb1
Sahusoft <sahusoft@Sahusofts-MacBook-Air-5.local>
2016-12-16 15:54:37 +0530

    ui

--- index.android.js
+++ index.android.js
@@ -3,6 +3,7 @@ import { TouchableHighlight, Text, View ,AppRegistry,StyleSheet,TextInput, Image
 import { Container, Header, Title, Content, Button, Footer,Input, List, ListItem, CardItem, Card,InputGroup } from 'native-base';
 import {observer} from 'mobx-react/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Micon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Message extends Component {
 
@@ -37,8 +38,8 @@ export default class Message extends Component {
     if(this.state.mobile.length < 10){
       alert("Invalid mobile number");
       this.setState({ mobile: '' });
-      msgStore.addMobileNumber(this.state.mobile);
-      msgStore.swipeScreen(two);
+      // msgStore.addMobileNumber(this.state.mobile);
+      // msgStore.swipeScreen(two);
     }
   }
   //<Image source={require('./Images/l1.png')} style={styles.logoImage}>
@@ -69,23 +70,55 @@ export default class Message extends Component {
                           <Text>
                               Please confirm your country code and enter your phone number.
                           </Text>
-                          <InputGroup borderType='underline' >
+                          <View style={{flexDirection: 'column', borderBottomWidth:1, borderBottomColor:'grey', justifyContent:'center',marginBottom:10}}>
+                              <Text style={{textAlign:'center',lineHeight:30, fontWeight:'bold', alignItems:'center'}}>
+                                India
 
-                            <Text>India</Text>
-                          </InputGroup>
-                          <TextInput
-                            autoCapitalize={'none'}
-                            maxLength={10}
-                            placeholder='Phone Number'
-                            value={this.state.mobile}
-                            onChangeText={(mobile) => this.onChanged(mobile)}
+                              </Text>
+                              <Micon style={{fontSize: 10, color: 'grey', alignItems:'flex-start',textAlign:'right'}} name= 'network-cell'/>
+                          </View>
+                          <View style={{flexDirection: 'row', flexWrap:'wrap', justifyContent:'flex-start',alignItems:'center', alignSelf:'stretch'}}>
+                              <Text style={{flex:0.5,
+                                            fontWeight:'bold',
+                                            alignItems:'flex-end',
+                                            color:'grey',
+                                            marginTop:4
+                                          }}
+                              > + </Text>
+                              <Text style={{flex:1,
+                                            fontWeight:'bold',
+                                            alignSelf:'center',
+                                            borderBottomWidth:1,
+                                            borderBottomColor:'grey',
+                                            justifyContent:'center',
+                                            textAlign:'center',
+                                            marginTop:16,
+                                            paddingBottom: 10,
+                                            marginRight:2,
 
-                          />
-                          <Button primary
-                          onPress={() => this.onSubmit()}
-                          style={{marginTop:10 ,marginBottom:10 ,marginLeft:100 ,padding:5,justifyContent:'flex-end', backgroundColor:'#D1D1D1'}}>
-                            Ok
-                          </Button>
+                                          }}
+                              > 91 </Text>
+                              <TextInput
+                                    autoCapitalize={'none'}
+                                    maxLength={10}
+                                    placeholder='Phone Number'
+                                    value={this.state.mobile}
+                                    onChangeText={(mobile) => this.onChanged(mobile)}
+                                    style={{flex:6,alignItems:'center',alignSelf:'center',justifyContent:'center',textAlign:'center'}}
+                              />
+                              <Button primary
+                                  onPress={() => this.onSubmit()}
+                                  style={{flex:1.5,
+                                          alignItems:'center',
+                                          alignSelf:'center',
+                                          justifyContent:'center',
+                                          backgroundColor:'#D1D1D1'
+                                        }}
+                              > Ok </Button>
+
+                          </View>
+
+
 
                       </CardItem>
                   </Card>
